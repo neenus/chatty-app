81d66bd7fd8ba7eaefd22867344be7e02640a76a
Neenus Gabriel <neenus@live.ca>
2018-09-06 00:03:29 +0000

    initial state set, passed current user and messages as props

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,9 +2,10 @@ import React, {Component} from 'react';
 
 export default class ChatBar extends Component {
   render(){
+    
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" defaultValue={this.props.users} placeholder="Your Name (Optional)" />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
--- src/messageList.jsx
+++ src/messageList.jsx
@@ -3,9 +3,13 @@ import Message from './Message.jsx';
 
 export default class MessageList extends Component{
   render() {
+    console.log(this.props.messages)
+    const message = this.props.messages.map((message, i) => 
+      {return (<Message key={i} message={message.content} username={message.username} />);}
+    )
     return(
       <main className="messages">
-        <Message />
+        {message}
       </main>
     );
   }
